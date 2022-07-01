152b7a667c841562158c5185034605ce494894c5
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-07-01 08:49:03 -0700

    feat(card component): create base of properties and methods to handle the flip of cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,27 @@
 import './styles.css'
 
-export function Card() {
+export interface CardProps {
+  id: string;
+  flipped?: boolean;
+  back: string;
+  handleClick?: (id: string) => void;
+}
+
+export function Card({ id, flipped = false, back, handleClick}: CardProps) {
+  const cardContentClassNames = ['card__content']
+  flipped && cardContentClassNames.push('card__content--flipped');
+
+  const handleClickFn = (id: string) => {
+    if (handleClick) {
+      handleClick(id)
+    }
+  }
+
   return (
-    <div className="card">
-      <div className="card__content">
+    <div className="card" onClick={() => handleClickFn(id)}>
+      <div className={cardContentClassNames.join(' ')}>
         <div className="card__face card__face--front">?</div>
-        <div className="card__face card__face--back">🐍</div>
+        <div className="card__face card__face--back">{back}</div>
       </div>
     </div>
   )
--- src/templates/App/index.tsx
+++ src/templates/App/index.tsx
@@ -1,8 +1,18 @@
 import { Card } from '../../components/Card'
 import './styles.css'
 
+const handleClick = (id: string) => {
+  console.log(id)
+}
+
 export function App() {
-  return <Card />
+  return (
+    <div className="app">
+      <Card id="1" back="🐍" flipped handleClick={handleClick} />
+      <Card id="2" back="🐍" handleClick={handleClick} />
+      <Card id="3" back="🐍" flipped handleClick={handleClick} />
+    </div>
+  )
 }
 
 export default App
