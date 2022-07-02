fce3ebd5270300f01ad004e3f106c5b700fe7c4e
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-07-02 12:14:59 -0700

    feat(cards): check if flipped cards match and unflip if don't match

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { duplicateRegenerateSortArray } from '../../utils/card-utils';
 import { Card, CardProps } from '../Card'
 import './styles.css'
@@ -11,6 +11,9 @@ export function Grid({cards}: GridProps) {
   const [stateCards, setStateCards] = useState(() => {
     return duplicateRegenerateSortArray(cards)
   })
+  const firstSelectedCard = useRef<CardProps | null>(null)
+  const secondSelectedCard = useRef<CardProps | null>(null)
+  const unflipCards = useRef(false)
 
   const handleClick = (id: string) => {
     const newStateCards = stateCards.map((card: CardProps) => {
@@ -20,8 +23,37 @@ export function Grid({cards}: GridProps) {
       // Se o card não estver virado, não faz nada
       if (card.flipped) return card;
 
+      // Desvira possíveis cards errados (diferentes entre si)
+      if (unflipCards.current && firstSelectedCard.current && secondSelectedCard.current) {
+        // O jogador errou
+        firstSelectedCard.current.flipped = false
+        secondSelectedCard.current.flipped = false
+        firstSelectedCard.current = null
+        secondSelectedCard.current = null
+        unflipCards.current = false
+      }
+
       // Vira o card
       card.flipped = true;
+
+      // Configura primeiro e segundo card clicados
+      if (firstSelectedCard.current === null) {
+        firstSelectedCard.current = card;
+      } else if (secondSelectedCard.current === null) {
+        secondSelectedCard.current = card;
+      }
+
+      // Se os dois cards estiverem virados, checar se são iguais
+      if (firstSelectedCard.current && secondSelectedCard.current) {
+        if (firstSelectedCard.current.back === secondSelectedCard.current.back) {
+          // O jogador acertou
+          firstSelectedCard.current = null
+          secondSelectedCard.current = null
+        } else {
+          // O jogador errou
+          unflipCards.current = true
+        }
+      }
 
       return card;
     })
