318a7936a8744b0b28afc3212786f8981936c908
Jeanne Lima <23726423+JeanneLima@users.noreply.github.com>
2022-07-02 14:12:25 -0300

    Develop - Add challenges from Otavio Miranda's tutorial (#5)
    * chore(base structure): create project base with vite, react and typescript
    
    * refactor(base structure): improve the organization of the project's base structure
    
    * feat(card component): create base structure and styling of card component
    
    * feat(card component): create base of properties and methods to handle the flip of cards
    
    * feat(card): create mockad data and methods for card duplication
    
    * feat(cards grid): create the grid component to adjust the layout of the elements on the screen
    
    * feat(cards): handle the cards flip event
    
    * feat(cards): check if flipped cards match and unflip if don't match
    
    * feat(cards): record the number of identical cards the player has hit so far, as well as the number of attempts made by the player
    
    * feat(cards): allow player to reset game
    
    Co-authored-by: Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>

---
+++ src/components/Grid/index.tsx
@@ -0,0 +1,94 @@
+import { useRef, useState } from 'react';
+import { duplicateRegenerateSortArray } from '../../utils/card-utils';
+import { Card, CardProps } from '../Card'
+import './styles.css'
+
+export interface GridProps {
+  cards: CardProps[];
+}
+
+export function Grid({cards}: GridProps) {
+  const [stateCards, setStateCards] = useState(() => {
+    return duplicateRegenerateSortArray(cards)
+  })
+  const firstSelectedCard = useRef<CardProps | null>(null)
+  const secondSelectedCard = useRef<CardProps | null>(null)
+  const unflipCards = useRef(false)
+  const [matches, setMatches] = useState(0)
+  const [moves, setMoves] = useState(0)
+
+  const handleResetGame = () => {
+    setStateCards(duplicateRegenerateSortArray(cards))
+    firstSelectedCard.current = null
+    secondSelectedCard.current = null
+    unflipCards.current = false
+    setMatches(0)
+    setMoves(0)
+  }
+
+  const handleClick = (id: string) => {
+    const newStateCards = stateCards.map((card: CardProps) => {
+      // Se o id do card não for o clicado, náo faz nada
+      if (card.id !== id) return card;
+
+      // Se o card não estver virado, não faz nada
+      if (card.flipped) return card;
+
+      // Desvira possíveis cards errados (diferentes entre si)
+      if (unflipCards.current && firstSelectedCard.current && secondSelectedCard.current) {
+        // O jogador errou
+        firstSelectedCard.current.flipped = false
+        secondSelectedCard.current.flipped = false
+        firstSelectedCard.current = null
+        secondSelectedCard.current = null
+        unflipCards.current = false
+      }
+
+      // Vira o card
+      card.flipped = true;
+
+      // Configura primeiro e segundo card clicados
+      if (firstSelectedCard.current === null) {
+        firstSelectedCard.current = card;
+      } else if (secondSelectedCard.current === null) {
+        secondSelectedCard.current = card;
+      }
+
+      // Se os dois cards estiverem virados, checar se são iguais
+      if (firstSelectedCard.current && secondSelectedCard.current) {
+        if (firstSelectedCard.current.back === secondSelectedCard.current.back) {
+          // O jogador acertou
+          firstSelectedCard.current = null
+          secondSelectedCard.current = null
+          setMatches(matchesCount => matchesCount + 1)
+        } else {
+          // O jogador errou
+          unflipCards.current = true
+        }
+
+        // A cada par de cards selecionados é contado 1 movimento do jogador
+        setMoves(movesCount => movesCount + 1)
+      }
+
+      return card;
+    })
+
+    setStateCards(newStateCards)
+  }
+
+  return (
+    <>
+      <div className="text">
+        <h1>Memory Game</h1>
+        <p>
+          Moves: {moves} | Matches: {matches} | <button onClick={() => handleResetGame()}>Reset</button>
+        </p>
+      </div>
+      <div className="grid">
+        {stateCards.map((card: CardProps) => {
+          return <Card {...card} key={card.id} handleClick={handleClick} />
+        })}
+      </div>
+    </>
+  )
+}
---
+++ src/data/cards.ts
@@ -0,0 +1,18 @@
+export const cards = [
+  {
+    id: "1",
+    back: "🐰"
+  },
+  {
+    id: "2",
+    back: "🙈"
+  },
+  {
+    id: "3",
+    back: "🦁"
+  },
+  {
+    id: "4",
+    back: "🐼"
+  }
+]
